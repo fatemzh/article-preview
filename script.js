c5f7fc55254130757c7815377fc194269b3cad80
fatemzh <abid.fatem@hotmail.fr>
2024-07-29 14:30:22 +0200

    share button in responsive mode

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const shareBtn = document.getElementById("share-btn");
-    const shareBanner = document.getElementById("share-banner");
-    const beforeClick = document.getElementById("before-click");
-  
-    shareBtn.addEventListener("click", function() {
-      if (shareBanner.classList.contains("hidden")) {
-        shareBanner.classList.remove("hidden");
-        beforeClick.classList.add("hidden");
-      } else {
-        shareBanner.classList.add("hidden");
-        beforeClick.classList.remove("hidden");
-      }
-    });
-  });
-  
+const shareBtn = document.getElementById("share-btn");
+const shareBtnBanner = document.getElementById("share-btn-banner");
+const beforeClick = document.getElementById("before-click");
+const afterClick = document.getElementById("after-click");
+
+shareBtn.addEventListener("click", () => {
+    beforeClick.style.visibility = "hidden";
+    afterClick.style.visibility = "visible";
+});
+
+shareBtnBanner.addEventListener("click", () => {
+    beforeClick.style.visibility = "visible";
+    afterClick.style.visibility = "hidden";
+});
